f05f3c4a35d5e566aff670c8bf16fd433e136de2
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2023-03-12 23:04:17 +0200

    Updated

--- POM/pageObjects/homePage.py
+++ POM/pageObjects/homePage.py
@@ -3,8 +3,14 @@ from selenium.webdriver.common.by import By
 
 class HomePage():
     button_burger_id = "react-burger-menu-btn"
-    tag_logout_id="logout_sidebar_link"
 
+    text_title_class="app_logo"
+    image_shopping_cart_id = "shopping_cart_container"
+    selection_product_sort_class = "product_sort_container"
+    a_link_all_items_id = "inventory_sidebar_link"
+    a_link_about_id = "about_sidebar_link"
+    a_link_reset_app = "reset_sidebar_link"
+    a_link_logout_id = "logout_sidebar_link"
     def __init__(self,driver):
         self.driver=driver
 
@@ -12,4 +18,44 @@ class HomePage():
         self.driver.find_element(By.ID,self.button_burger_id).click()
 
     def clickLogout(self):
-        self.driver.find_element(By.ID,self.tag_logout_id).click()
+        self.driver.find_element(By.ID,self.a_link_logout_id).click()
+
+    def isTitleVisible(self):
+        return self.driver.find_element(By.CLASS_NAME, self.text_title_class).is_displayed()
+
+
+    def isShoppingCartVisible(self):
+        return self.driver.find_element(By.ID,self.image_shopping_cart_id).is_displayed()
+
+    def isHamburgerMenuVisible(self):
+        return self.driver.find_element(By.ID,self.button_burger_id).is_displayed()
+    def isHamburgerMenuEnable(self):
+        return self.driver.find_element(By.ID,self.button_burger_id).is_displayed()
+
+    def isAllItemsVisible(self):
+        return self.driver.find_element(By.ID,self.a_link_all_items_id).is_displayed()
+    def isAllItemsEnable(self):
+        return self.driver.find_element(By.ID,self.a_link_all_items_id).is_enabled()
+
+    def isAboutVisible(self):
+        return self.driver.find_element(By.ID, self.a_link_about_id).is_displayed()
+
+    def isAboutEnable(self):
+        return self.driver.find_element(By.ID, self.a_link_about_id).is_enabled()
+
+    def isLogOutVisible(self):
+        return self.driver.find_element(By.ID, self.a_link_logout_id).is_displayed()
+
+    def isLogOutEnable(self):
+        return self.driver.find_element(By.ID, self.a_link_logout_id).is_enabled()
+
+    def isResetAppVisible(self):
+        return self.driver.find_element(By.ID, self.a_link_reset_app).is_displayed()
+
+    def isResetAppEnable(self):
+        return self.driver.find_element(By.ID, self.a_link_reset_app).is_enabled()
+
+    def isItemSortVisible(self):
+        return self.driver.find_element(By.CLASS_NAME, self.selection_product_sort_class).is_displayed()
+
+
--- POM/testCases/SmokeTest.py
+++ POM/testCases/SmokeTest.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 from selenium import webdriver
 import sys
@@ -13,10 +14,11 @@ sys.tracebacklimit = 0
 __unittest = True
 
 
-class SmokeTest(unittest.TestCase):
+class LoginPageTest(unittest.TestCase):
     baseURL = "https://www.saucedemo.com/"
     standard_user = "standard_user"
     password = "secret_sauce"
+    homeURL = "https://www.saucedemo.com/inventory.html"
 
     '''
        Pre testing: Setting webdriver for Chrome.
@@ -33,44 +35,138 @@ class SmokeTest(unittest.TestCase):
     def test_isTitleVisible(self):
         self.driver.get(self.baseURL)
         lg = LoginPage(self.driver)
-        print(lg.isTitleVisible())
         self.assertEqual(True, lg.isTitleVisible())
 
     def test_isUsernameInputVisible(self):
         self.driver.get(self.baseURL)
         lg = LoginPage(self.driver)
-        print(lg.isUsernameInputVisible())
         self.assertEqual(True, lg.isUsernameInputVisible())
 
     def test_isUsernameInputEnable(self):
         self.driver.get(self.baseURL)
         lg = LoginPage(self.driver)
-        print(lg.isUsernameInputEnable())
         self.assertEqual(True, lg.isUsernameInputEnable())
 
     def test_isPasswordInputVisible(self):
         self.driver.get(self.baseURL)
         lg = LoginPage(self.driver)
-        print(lg.isPasswordInputVisible())
         self.assertEqual(True, lg.isPasswordInputVisible())
 
     def test_isPasswordInputEnable(self):
         self.driver.get(self.baseURL)
         lg = LoginPage(self.driver)
-        print(lg.isPasswordInputEnable())
         self.assertEqual(True, lg.isPasswordInputEnable())
-    def isLoginButtonVisible(self):
+
+    def test_isLoginButtonVisible(self):
         self.driver.get(self.baseURL)
         lg = LoginPage(self.driver)
-        print(lg.isLoginButtonVisible())
         self.assertEqual(True, lg.isLoginButtonVisible())
 
     def test_isLoginButtonEnable(self):
         self.driver.get(self.baseURL)
         lg = LoginPage(self.driver)
-        print(lg.isLoginButtonEnable())
         self.assertEqual(True, lg.isLoginButtonEnable())
+
+    '''
+       Step 1: Go to URL.
+       Step 2: Enter username: standard_user.
+       Step 3: Enter password: secret_password.
+       Step 4: Click on button Login.
+       Step 5: verify url.
+       Step 6: Click on Menu at the left side.
+       Step 7: Click on button Logout
+       '''
+
+    # def test_login_logout_with_standard_user(self):
+    #     self.driver.get(self.baseURL)
+    #     lg = LoginPage(self.driver)
+    #     lg.setUsername(self.standard_user)
+    #     lg.setPassword(self.password)
+    #     lg.clickLogin()
+    #     act_url = self.driver.current_url
+    #     exp_url = "https://www.saucedemo.com/inventory.html"
+    #     self.assertEqual(exp_url, act_url)
+    #     hp = HomePage(self.driver)
+    #     hp.clickHamburger()
+    #     hp.clickLogout()
 
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
+
+
+class HomePageTest(unittest.TestCase):
+    baseURL = "https://www.saucedemo.com/"
+    standard_user = "standard_user"
+    password = "secret_sauce"
+    homeURL = "https://www.saucedemo.com/inventory.html"
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+
+        cls.driver.get(cls.baseURL)
+        lg = LoginPage(cls.driver)
+        lg.setUsername(cls.standard_user)
+        lg.setPassword(cls.password)
+        lg.clickLogin()
+        act_url = cls.driver.current_url
+        exp_url = "https://www.saucedemo.com/inventory.html"
+        cls().assertEqual(exp_url, act_url)
+
+    def test_isLogoVisible(self):
+        hp = HomePage(self.driver)
+        self.assertEqual(True,hp.isTitleVisible())
+    def test_isLogoVisible(self):
+        hp = HomePage(self.driver)
+        self.assertEqual(True,hp.isTitleVisible())
+
+    def test_isShoppingCartVisible(self):
+        hp = HomePage(self.driver)
+        self.assertEqual(True,hp.isShoppingCartVisible())
+
+    def test_isHamburgerMenuEnable(self):
+        hp = HomePage(self.driver)
+        self.assertEqual(True,hp.isHamburgerMenuEnable())
+
+    def test_isHamburgerMenuVisible(self):
+        hp = HomePage(self.driver)
+        self.assertEqual(True, hp.isHamburgerMenuVisible())
+
+    def test_isItemSortVisible(self):
+        hp = HomePage(self.driver)
+        self.assertEqual(True, hp.isItemSortVisible())
+
+    def test_ElementsInHamburgerMenuVisible(self):
+        hp = HomePage(self.driver)
+
+        hp.clickHamburger()
+        time.sleep(1)
+        self.assertEqual(True, hp.isAllItemsVisible(),"All Items invisible")
+        self.assertEqual(True, hp.isAboutVisible(),"About invisible")
+        self.assertEqual(True, hp.isLogOutVisible(),"Logout invisible")
+        self.assertEqual(True, hp.isResetAppVisible(),"sResetApp invisible")
+        self.assertEqual(True, hp.isAllItemsEnable(),"All Items disable")
+        self.assertEqual(True, hp.isAboutEnable(),"About disable")
+        self.assertEqual(True, hp.isLogOutEnable(),"Logout disable")
+        self.assertEqual(True, hp.isResetAppEnable(),"Reset disable")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+# def suite():
+#     test_suite = unittest.TestSuite()
+#     test_suite.addTest((HomePageTest("test_all_elements_in_home_page")))
+#
+#     return test_suite
+#
+#
+# mySuite = suite()
+#
+# runner = unittest.TextTestRunner()
+#
+# runner.run(mySuite)
+if __name__ == '__main__':
+    unittest.main()
